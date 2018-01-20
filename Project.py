7a9d4ca6fde328bf8c9c1ae825dd3a4787b3ad9f
Ian Agpawa <agpawaji@gmail.com>
2018-01-20 13:40:59 -0500

    feat: Create Project class

---
+++ Project.py
@@ -0,0 +1,8 @@
+class Project():
+    def __init__(self, title, description, thumbnail, date):
+        self.title = title
+        self.description = description
+        self.thumbnail = thumbnail
+        self.date = date
+
+        
