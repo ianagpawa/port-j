716e8f0b0a7d573a46a9d08614229eae5b1c771d
Ian Agpawa <agpawaji@gmail.com>
2018-01-20 14:00:43 -0500

    refactor: Add projects template with test objects

---
+++ list_of_projects.py
@@ -0,0 +1,7 @@
+from Project import Project
+
+first = Project("first", 'testing', 'nothing', '3/14/15')
+
+second = Project('second', 'testing1', 'nothing2', '5/5/18')
+
+third = Project("third", 'testing2', 'nothing3', '9/9/10')
--- main.py
+++ main.py
@@ -10,6 +10,10 @@ import json
 from string import letters
 from functools import wraps
 
+
+from google.appengine.ext import ndb
+
+import list_of_projects
 # from google.appengine.ext import ndb
 
 APP_SECRET = json.loads(open('secrets.json', 'r').read())['app-secret-key']
@@ -20,3 +24,8 @@ app.secret_key = APP_SECRET
 @app.route('/', methods=['GET'])
 def main():
     return render_template('main.html')
+
+@app.route('/projects', methods=['GET'])
+def projects():
+    projects = [list_of_projects.first, list_of_projects.second, list_of_projects.third,list_of_projects.first, list_of_projects.second, list_of_projects.third,list_of_projects.first, list_of_projects.second, list_of_projects.third,]
+    return render_template("projects.html", projects=projects)
